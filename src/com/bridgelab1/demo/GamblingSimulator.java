72d7d0583a46ee46dadeee518653477937aca4b7
first_name <ss329222@gmail.com>
2022-02-09 18:41:42 +0530

    Added:stake and bet values

--- src/com/bridgelab1/demo/GamblingSimulator.java
+++ src/com/bridgelab1/demo/GamblingSimulator.java
@@ -1,6 +1,9 @@
 package com.bridgelab1.demo;
 
 public class GamblingSimulator {
+	public static final int STAKE_PER_DAY = 100;
+	public static final int BET_PER_GAME = 1;
+	
 
 	public static void main(String[] args)
 	{
