c6165f0bbd8b2659790ac7743c89bfac48e3a7d6
first_name <ss329222@gmail.com>
2022-02-09 18:52:32 +0530

    Added:Check win or lose

--- src/com/bridgelab1/demo/GamblingSimulator.java
+++ src/com/bridgelab1/demo/GamblingSimulator.java
@@ -1,13 +1,31 @@
 package com.bridgelab1.demo;
+import java.util.Random;
 
 public class GamblingSimulator {
 	public static final int STAKE_PER_DAY = 100;
 	public static final int BET_PER_GAME = 1;
+	public static final int WIN = 1;
+	public static int stake = 0;
+	public static void winCheck() {
+		Random random = new Random();
+		int betReturns = random.nextInt(2);
+		if (betReturns == WIN) {
+			stake++;// incrementing
+			System.out.println("win");
+		} else {
+			stake--;// decrementing
+			System.out.println("loss");
+		}
+		System.out.println("stake = " + stake);
+
+	}
+
 	
 
 	public static void main(String[] args)
 	{
 		System.out.println("wlcome to  gambling simulator");
+		winCheck();
 
 	}
 
